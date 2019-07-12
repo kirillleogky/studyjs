76892ec2be3993089012567b631e509b3c65bb04
Kirill Leogky <kirillleogky@gmail.com>
2019-07-12 14:47:40 +0300

    change task10

--- src/core/task10.js
+++ src/core/task10.js
@@ -5,13 +5,13 @@ export default function Number() {
     this.second = prompt("Введите второе число");
 
     this.plus = function() {
-        var plus = (+this.first) + (+this.second);
+        const plus = (+this.first) + (+this.second);
         if (isNaN(plus) || !plus) return "Неверные значения";
         return plus;
     };
 
     this.multiply = function() {
-        var plus = (+this.first) * (+this.second);
+        const plus = (+this.first) * (+this.second);
         if (isNaN(plus) || !plus) return "Неверные значения";
         return plus;
     };
