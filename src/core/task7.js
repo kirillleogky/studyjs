5dab52f129c3eeb276ba50a25c14f74c04a47486
Kirill Leogky <kirillleogky@gmail.com>
2019-07-18 18:02:45 +0300

    change task7

--- src/core/task7.js
+++ src/core/task7.js
@@ -7,7 +7,7 @@ export default function compare(first, second) {
 
 function exponentiation(first, second) {
 
-    if (first < 0 && second < 0) return "Invalid Value";
-    return first ** second;
+  if (second < 0) return "Invalid Value";
+  return first ** second;
 
 }
